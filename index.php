d031129426c3df8204e159e73cc97c7b95b69bf5
skarpov <sergo-8ck@yandex.ru>
2019-05-17 17:38:23 +0300

    frontend

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Тестовое задание tutor.ru</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div id="main">
+
+    <div id="addReviewContainer">
+        <p>Добавить отзыв</p>
+        <form id="addReviewForm" method="post" action="">
+            <div>
+                <label for="name">Ваше имя</label>
+                <input type="text" name="name" id="name" />
+
+                <label for="email">Ваш Email</label>
+                <input type="text" name="email" id="email" />
+
+                <label for="text">Отзыв</label>
+                <textarea name="text" id="text" cols="20" rows="5"></textarea>
+
+                <input type="submit" id="submit" value="Submit" />
+            </div>
+        </form>
+    </div>
+</div>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
